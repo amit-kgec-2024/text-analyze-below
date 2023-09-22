fdada879e410631adb12d2f361224632b02134cd
Amit Mandal <138322339+amit-mandal-kgce@users.noreply.github.com>
2023-09-23 04:17:17 +0530

    videos 10 harry

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+// import About from './components/About';
 import TextArea from './components/TextArea';
 
 function App() {
   return (
     <>
     <Navbar title="IndMart" about="About" contact="Contact"/>
-    <div className="container">
-      <TextArea heading="Ener The text to analyze below"/>
-    </div>
+    <div className="container"><TextArea heading="Ener The text to analyze below"/></div>
+    {/* <About/> */}
     </>
   );
 }
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,31 +1,68 @@
 import { useState } from "react";
 
 export default function TextArea(props) {
-    const [text, setText] = useState('Enter your Text')
+    const [text, setText] = useState('')
 
     const handelUpClick = () => {
-        // console.log("Upper case was Clicked" + text)
-        
         let newText = text.toUpperCase();
         setText(newText)
     }
+    const handelLoClick = () => {
+      let newText = text.toLowerCase();
+      setText(newText);
+    };
+    const handelCapitalizedClick = () => {
+      let newText = text[0].toUpperCase() + text.slice(1);
+      setText(newText);
+    };
+    const textClear = () => {
+      let newText = (" ");
+      setText(newText);
+    };
     const handelOnChange = (event) => {
-        // console.log("On Change")
         setText(event.target.value)
     }
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div class="mb-3">
-        <textarea
-          class="form-control"
-          id="myBox"
-          value={text}
-          onChange={handelOnChange}
-          rows="8"
-        ></textarea>
-        <button className="btn btn-primary" onClick={handelUpClick}>Convert to Uppercase</button>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div class="mb-3">
+          <textarea
+            class="form-control"
+            id="myBox"
+            value={text}
+            onChange={handelOnChange}
+            rows="8"
+          ></textarea>
+          <button className="btn btn-primary my-3 mx-1" onClick={handelUpClick}>
+            Convert to Uppercase
+          </button>
+          <button
+            className="btn btn-secondary my-3 mx-1"
+            onClick={handelLoClick}
+          >
+            Convert to Lowercase
+          </button>
+          <button
+            className="btn btn-secondary my-3 mx-1"
+            onClick={handelCapitalizedClick}
+          >
+            Capitalized Case
+          </button>
+          <button className="btn btn-danger my-3 mx-1" onClick={textClear}>
+            Clear
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="container my-3">
+        <h1>Your text Summery</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length}Minutes Reads</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
